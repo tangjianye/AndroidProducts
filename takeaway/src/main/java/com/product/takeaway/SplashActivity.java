fca827160c660d305609193edbd46de94d2e915c
jianye.tang <jianye.tang@aorise.org>
2017-06-30 15:53:36 +0800

    增加命名

--- takeaway/src/main/java/com/product/takeaway/SplashActivity.java
+++ takeaway/src/main/java/com/product/takeaway/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+@Deprecated
 public class SplashActivity extends AppCompatActivity {
     private static final int DELAY_MILLIS = 2000;
 
